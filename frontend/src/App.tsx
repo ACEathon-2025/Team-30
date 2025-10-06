ec75398d5afcaaad1f919679231ac9be44dab07b
A Anish Bhat <144614131+Anish17Bhat@users.noreply.github.com>
2025-10-06 16:16:41 +0530

    frontend slider component created to make api calls

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import TrafficSliderInput from './components/TrafficSliderInput'
 
 function App() {
   return (
     <>
-      <p>Hello</p>
+      <p>Hello this is the traffic slider</p>
+      <TrafficSliderInput/>
     </>
   )
 }
